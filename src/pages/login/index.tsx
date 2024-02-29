10cd33ad3edca72cb4c5177c0f3ea5df9bcc0b0a
herlianali <aliyasa58@gmail.com>
2024-02-29 10:45:09 +0700

    update dashboard

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../../../styles/globals.css";
-import Image from 'next/image'
+// import Image from 'next/image'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +15,7 @@ export default function LoginPage() {
         <>
           <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-              <Image
+              <img
                 className="mx-auto h-10 w-auto"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 alt="Your Company"
@@ -25,66 +25,64 @@ export default function LoginPage() {
               </h2>
             </div>
     
-            {/* <div className=""> */}
-              <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm rounded overflow-hidden shadow-lg bg-white p-3">
-                <form className="space-y-6" action="#" method="POST">
-                  <div>
-                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                      Email address
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm rounded overflow-hidden shadow-lg bg-white p-3">
+              <form className="space-y-6" action="#" method="POST">
+                <div>
+                  <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                    Email address
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+    
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                      Password
                     </label>
-                    <div className="mt-2">
-                      <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        autoComplete="email"
-                        required
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      />
+                    <div className="text-sm">
+                      <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                        Forgot password?
+                      </a>
                     </div>
                   </div>
-      
-                  <div>
-                    <div className="flex items-center justify-between">
-                      <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                        Password
-                      </label>
-                      <div className="text-sm">
-                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                          Forgot password?
-                        </a>
-                      </div>
-                    </div>
-                    <div className="mt-2">
-                      <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        autoComplete="current-password"
-                        required
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      />
-                    </div>
+                  <div className="mt-2">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
                   </div>
-      
-                  <div>
-                    <button
-                      type="submit"
-                      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                      Sign in
-                    </button>
-                  </div>
-                </form>
-      
-                <p className="mt-10 text-center text-sm text-gray-500">
-                  Not a member?{' '}
-                  <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                    Start a 14 day free trial
-                  </a>
-                </p>
-              </div>
-            {/* </div> */}
+                </div>
+    
+                <div>
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Sign in
+                  </button>
+                </div>
+              </form>
+    
+              <p className="mt-10 text-center text-sm text-gray-500">
+                Not a member?{' '}
+                <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                  Start a 14 day free trial
+                </a>
+              </p>
+            </div>
           </div>
         </>
     )
